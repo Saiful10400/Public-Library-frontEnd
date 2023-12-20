f8c5afbb5b23f780a9e53f2eeebadef4fe05433e
saiful10400 <saifulislam10400i5@gmail.com>
2023-12-20 13:20:06 +0600

    minor change on book adding page.

--- src/Components/Other/UserDashboard/UserDashboardRoot.jsx
+++ src/Components/Other/UserDashboard/UserDashboardRoot.jsx
@@ -1,14 +1,19 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "./userDashboard.css"
+import { FaUserEdit } from "react-icons/fa";
+
+
+
+
 const UserDashboardRoot = () => {
   const li = (
     <>
       
       <li>
-        <NavLink to={"/User_dashboard/Update_profile"}>Update Profile</NavLink>
+        <NavLink className="border  w-full flex  gap-3 items-center py-[10px] px-[15px] " to={"/User_dashboard/Update_profile"}><FaUserEdit className="text-xl" /> Update Profile</NavLink>
       </li>
       <li>
-        <NavLink to={"/User_dashboard/Add_a_book"}>Add a book</NavLink>
+        <NavLink className="border  w-full flex  gap-3 items-center py-[10px] px-[15px] " to={"/User_dashboard/Add_a_book"}>Add a book</NavLink>
       </li>
     </>
   );
@@ -17,7 +22,7 @@ const UserDashboardRoot = () => {
       <div>
         <div className="lg:flex gap-4">
           <div className="lg:w-[20%] ">
-            <ul>
+            <ul className="flex flex-col gap-2">
                 {li}
             </ul>
           </div>
