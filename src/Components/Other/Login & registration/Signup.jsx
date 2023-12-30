f21eb798e6332ee6164acfdb2f497bb2650b90a9
saiful10400 <saifulislam10400i5@gmail.com>
2023-12-30 16:48:59 +0600

    signup page all functionality done

--- src/Components/Other/Login & registration/Signup.jsx
+++ src/Components/Other/Login & registration/Signup.jsx
@@ -5,74 +5,83 @@ import { FaArrowRightLong } from "react-icons/fa6";
 import { FaArrowLeft } from "react-icons/fa";
 import SharedHader from "./shared component/SharedHader";
 import { useState } from "react";
-import lady from "../../../../public/lady.jpg"
-import gents from "../../../../public/gents.png"
+import lady from "../../../../public/lady.jpg";
+import gents from "../../../../public/gents.png";
+import useImgUpload from "../../custom Hooks/useImgUpload";
 
-let user={
-  firstName:undefined,
-  lastName:undefined,
-  gender:undefined,
-  email:undefined,
-  password:undefined
-}
+let user = {
+  firstName: undefined,
+  lastName: undefined,
+  gender: undefined,
+  email: undefined,
+  password: undefined,
+  photoUrl:undefined
+};
 
 const Signup = () => {
   // input type style classes.
-  const inputStyle="w-full focus:outline-none border textarea-lg lg:text-xl py-2 lg:py-3  px-3 font-normal rounded-md"
-  let [progress,setProgress]=useState(["step-primary"])
-  console.log(progress)
-  let[reload,setReload]=useState(true)
-  let[profile,setProfile]=useState({file:undefined,string:undefined})
+  const inputStyle =
+    "w-full focus:outline-none border textarea-lg lg:text-xl py-2 lg:py-3  px-3 font-normal rounded-md";
+  let [progress, setProgress] = useState(["step-primary"]);
+  console.log(progress);
+  let [reload, setReload] = useState(true);
+  let [profile, setProfile] = useState({ file: undefined, string: undefined });
 
   // user all data managing form here.
 
- 
-  
-  
   // 1.users info
   // userfirstname
-  const firstName=(e)=>{
-    user.firstName=e.target.value
-    setReload(!reload)
-    
-  }
+  const firstName = (e) => {
+    user.firstName = e.target.value;
+    setReload(!reload);
+  };
   // user last name.
-  const lastName=(e)=>{
-    user.lastName=e.target.value
-    setReload(!reload)
-  }
+  const lastName = (e) => {
+    user.lastName = e.target.value;
+    setReload(!reload);
+  };
   // user gender
-  const gender=(e)=>{
-    user.gender=e.target.value
-    setReload(!reload)
-    console.log(user)
-     
-  }
+  const gender = (e) => {
+    user.gender = e.target.value;
+    setReload(!reload);
+    console.log(user);
+  };
   // 2.email and password.
   // email
-  const email=(e)=>{
-    user.email=e.target.value
-    setReload(!reload)
-    
-  }
+  const email = (e) => {
+    user.email = e.target.value;
+    setReload(!reload);
+  };
   // password
-  const password=(e)=>{
-    user.password=e.target.value
-    setReload(!reload)
-    
-    
-  }
+  const password = (e) => {
+    user.password = e.target.value;
+    setReload(!reload);
+  };
   // 3.profile photo upload.
-  const profilePhoto=(e)=>{
-    const uploadedfile=e.target.files[0]
-
-    const reader=new FileReader()
-    reader.onload=()=>{
-      setProfile({file:uploadedfile,string:reader.result})
-    }
-    reader.readAsDataURL(uploadedfile)
+  const imgbb = useImgUpload();
+  let [uploadLoader,setUploadLoader]=useState(false)
+  const profilePhoto = (e) => {
+    const uploadedfile = e.target.files[0];
+    
+    const reader = new FileReader();
+    reader.onload = () => {
+      setProfile({ file: uploadedfile, string: reader.result });
+    };
+    reader.readAsDataURL(uploadedfile);
+    setUploadLoader(true)
+    imgbb(uploadedfile)
+      .then((res) => res.json())
+      .then((data) => {
+        setUploadLoader(false)
+        user.photoUrl=data.data.url
+        
+      });
+  };
+  // Signup handle submit.
+  const signUpHandle=()=>{
+    console.log(user,"from function.")
   }
-  console.log(profile)
+  console.log(user);
   return (
     <div>
       <div className="flex lg:flex-row flex-col justify-center items-center h-[90vh]">
@@ -84,65 +93,120 @@ const Signup = () => {
             <div>
               <ul className="steps gap-5 font-bold text-gray-500">
                 <li className={`step ${progress[0]}`}>Personal Info</li>
-                <li className={`step ${progress[1]}`}>E-mail & password   </li>
+                <li className={`step ${progress[1]}`}>E-mail & password </li>
                 <li className={`step ${progress[2]}`}>Profile photo</li>
-               
               </ul>
-              
+
               {/* relative data container */}
               <div className="min-h-[30vh] flex justify-center items-center">
-
                 {/* personal info. */}
-                <div className={progress.length===1?"block":"hidden"}>
-                 <input onChange={firstName} type="text" className={inputStyle} placeholder="First name" />
-                 <input onChange={lastName} type="text" className={inputStyle+" "+"my-4"} placeholder="Last name" />
-                 <select onChange={gender} className={inputStyle}>
-                  <option disabled selected>Select gender.</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                 </select>
+                <div className={progress.length === 1 ? "block" : "hidden"}>
+                  <input
+                    onChange={firstName}
+                    type="text"
+                    className={inputStyle}
+                    placeholder="First name"
+                  />
+                  <input
+                    onChange={lastName}
+                    type="text"
+                    className={inputStyle + " " + "my-4"}
+                    placeholder="Last name"
+                  />
+                  <select onChange={gender} className={inputStyle}>
+                    <option disabled selected>
+                      Select gender.
+                    </option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
                 </div>
 
                 {/* email and password. */}
-                <div className={progress.length===2?"block":"hidden"}>
-                 <input onChange={email} type="email" className={inputStyle} placeholder="Your e-mail." />
-                 <input onChange={password} type="password" className={inputStyle+" "+"mt-4"} placeholder="minimum 6 digit password" />
+                <div className={progress.length === 2 ? "block" : "hidden"}>
+                  <input
+                    onChange={email}
+                    type="email"
+                    className={inputStyle}
+                    placeholder="Your e-mail."
+                  />
+                  <input
+                    onChange={password}
+                    type="password"
+                    className={inputStyle + " " + "mt-4"}
+                    placeholder="minimum 6 digit password"
+                  />
                 </div>
 
                 {/* profile photo upload. */}
-                <div className={progress.length===3?"block":"hidden"}>
-                 <label htmlFor="profile">
-                  <div className="border h-[150px] w-[150px] rounded-full relative overflow-hidden">
-                    <span className={`bg-gray-200 absolute bottom-0 left-0 w-full text-center h-[30px] font-bold ${profile.string?"hidden":"block"}`}>upload</span>
-                    <img className="w-full h-full object-contain" src={profile.string?profile.string:(user.gender==="female"?lady:gents)} alt="" />
-                  </div>
-                 </label>
-                 <input onChange={profilePhoto} id="profile"  type="file" className="hidden" />
+                <div className={progress.length === 3 ? "block" : "hidden"}>
+                  <label htmlFor="profile">
+                    <div className="border h-[150px] cursor-pointer w-[150px] rounded-full relative overflow-hidden">
+                      <span
+                        className={`bg-gray-200 absolute bottom-0 left-0 w-full text-center h-[30px] font-bold ${
+                          profile.string ? "hidden" : "block"
+                        }`}
+                      >
+                        upload
+                      </span>
+                      <img
+                        className="w-full h-full object-contain"
+                        src={
+                          profile.string
+                            ? profile.string
+                            : user.gender === "female"
+                            ? lady
+                            : gents
+                        }
+                        alt=""
+                      />
+                      <div className={`absolute ${uploadLoader?"flex": "hidden"} top-0 left-0 h-full  justify-center items-center bg-[#ffffffc0] w-full`}><span className="loading loading-spinner text-primary"></span></div>
+                    </div>
+                    
+                  </label>
+                  <input accept="image/png, image/gif, image/jpeg" disabled={uploadLoader}
+                    onInput={profilePhoto}
+                    id="profile"
+                    type="file"
+                    className="hidden"
+                  />
                 </div>
-
               </div>
 
               {/* navigation buttons. */}
               <div className="flex gap-2 items-center">
-                <button onClick={()=>{
-                  if(progress.length>=2){
-                    progress.pop()
-                  setProgress([...progress])
-                  }
-                }} className="w-1/5 btn btn-primary">
+                <button
+                  onClick={() => {
+                    if (progress.length >= 2) {
+                      progress.pop();
+                      setProgress([...progress]);
+                    }
+                  }}
+                  className="w-1/5 btn btn-primary"
+                >
                   <FaArrowLeft></FaArrowLeft>
                 </button>
                 <button
-                disabled={user.firstName&&user.lastName&&user.gender&&progress.length===1 || user.email&&user.password&&progress.length===2?false:true}
-                
-                  onClick={() =>{
-                    if(progress.length<3){
-                      setProgress([...progress,"step-primary"])
+                  disabled={
+                    (user.firstName &&
+                      user.lastName &&
+                      user.gender &&
+                      progress.length === 1) ||
+                    (user.email && user.password && progress.length === 2) || progress.length===3&&user.photoUrl&&!uploadLoader
+                      ? false
+                      : true
+                  }
+                  onClick={() => {
+                    if (progress.length < 3) {
+                      setProgress([...progress, "step-primary"]);
+                    }
+                    else if(progress.length===3){
+                      signUpHandle()
                     }
                   }}
                   className="w-4/5 btn btn-primary"
                 >
-                  Next <FaArrowRightLong></FaArrowRightLong>
+                  {progress.length===3?"Sign up":<span className="flex gap-2">Next <FaArrowRightLong></FaArrowRightLong></span>}
                 </button>
               </div>
             </div>
